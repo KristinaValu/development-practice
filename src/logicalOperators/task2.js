64551f12e41906396e275ee77baac4b597a7e651
Kristina <christkristina@yahoo.com>
2022-11-23 10:48:50 +0200

    JavaScript Logical Operators

--- src/logicalOperators/task2.js
+++ src/logicalOperators/task2.js
@@ -5,4 +5,6 @@ if (a.length > 6 && firstLetter.toUpperCase() === firstLetter) {
   console.log("Long uppercase word");
 } else if (a.length <= 6 && firstLetter.toLowerCase() === firstLetter) {
   console.log("Short lowercase word");
-} else console.log("Short uppercase or long lowercase");
+} else {
+  console.log("Short uppercase or long lowercase");
+}
